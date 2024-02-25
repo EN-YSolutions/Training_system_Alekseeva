10e67eb7ec4f6925719b0cef1edf3322a4c79ec5
Tyler <nataalex223@gmail.com>
2024-02-25 23:12:16 +0300

    Профиль пользователя. Минимальная настройка

--- app/routes/profile.py
+++ app/routes/profile.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import current_user
 
 profile_bp = Blueprint('profile', __name__)
 
 
-@profile_bp.route('/')
+@profile_bp.route('/profile')
 def profile():
-    return render_template("profile/profile.html", user=current_user)
+    menu_type = request.args.get('menu')
+
+    return render_template("profile/profile.html", user=current_user, menu_type=menu_type)
