c98a8471d607313be580cb6aa6350156815c011c
Tyler <nataalex223@gmail.com>
2024-03-01 12:52:11 +0300

    На вкладке 'Настройки' теперь можно изменить данные пользователя. В панель навигации добавлен отдельный значек для уведомлений

--- app/routes/profile.py
+++ app/routes/profile.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify, flash
 from flask_login import current_user
 from flask_bcrypt import check_password_hash
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
@@ -10,48 +10,14 @@ from app.static.python.identicons import generate_avatar
 profile_bp = Blueprint('profile', __name__)
 
 
-@profile_bp.route('/profile')
+@profile_bp.route('/profile', methods=['GET', 'POST'])
 def profile():
     if not current_user.is_authenticated:
         return redirect(url_for('auth.login'))
 
     menu_type = request.args.get('menu')
 
-    settings = SettingsForm()
-    settings.scoring_system.choices = [
-        (1, 'Абстрактная'), (2, 'Числовые значения')]
-
-    scoring_system_names = {1: 'abstract', 2: 'points'}
-
-    if request.method == 'POST':
-        if settings.validate_on_submit():
-            try:
-                user_name = settings.user_name.data
-                old_password = settings.old_password.data
-                new_password = settings.new_password.data
-                confirm_password = settings.confirm_password.data
-                scoring_system = settings.scoring_system.data
-
-                if user_name != current_user.name:
-                    current_user.name = user_name
-
-                if check_password_hash(current_user.password, old_password):
-                    current_user.password = bcrypt.generate_password_hash(
-                        new_password)
-
-                if scoring_system_names[scoring_system] != current_user.scoring_system:
-                    current_user.scoring_system = scoring_system_names[scoring_system]
-
-                db.session.commit()
-            except IntegrityError as e:
-                db.session.rollback()
-                print(str(e))
-
-            except SQLAlchemyError as e:
-                db.session.rollback()
-                print(str(e))
-
-    return render_template("profile/profile.html", user=current_user, menu_type=menu_type, settings_form=settings)
+    return render_template("profile/profile.html", user=current_user, menu_type=menu_type)
 
 
 @profile_bp.route("/profile/upload_image", methods=['POST'])
@@ -61,7 +27,7 @@ def upload_image():
 
     if 'image' not in request.files:
         return jsonify({'category': 'danger', 'message': 'Нет файла с изображением'})
-    
+
     file = request.files['image']
 
     if file:
@@ -81,19 +47,65 @@ def upload_image():
         if image.mode != 'RGB':
             image = image.convert('RGB')
 
-        image.save(f'app/static/images/users/{current_user.login}.jpg', format="JPEG")
+        image.save(
+            f'app/static/images/users/{current_user.login}.jpg', format="JPEG")
 
         return jsonify({'category': 'success', 'message': 'Фото отправлено'})
-    
+
     else:
 
         return jsonify({'category': 'danger', 'message': 'Ошибка на сервере'})
-    
+
 
 @profile_bp.route("/profile/delete_image", methods=['DELETE'])
 def delete_image():
-    
+
     image = generate_avatar(current_user.login)
-    image.save(f'app/static/images/users/{current_user.login}.jpg', format='jpeg')
+    image.save(
+        f'app/static/images/users/{current_user.login}.jpg', format='jpeg')
 
     return jsonify({'category': 'success', 'message': 'Фото удалено'})
+
+
+@profile_bp.route("/profile/update_settings", methods=['POST'])
+def update_settings():
+
+    scoring_system_names = {1: 'abstract', 2: 'points'}
+
+    try:
+        new_name = request.form.get('new-name')
+        scoring_system = int(request.form.get('scoring-system'))
+        old_password = request.form.get('old-password')
+        new_password = request.form.get('new-password')
+        confirm_password = request.form.get('confirm-password')
+
+        if new_name and new_name != current_user.name:
+            current_user.name = new_name
+
+        if scoring_system_names.get(scoring_system, 'points') != current_user.scoring_system:
+            current_user.scoring_system = scoring_system_names[scoring_system]
+
+        if old_password:
+            if check_password_hash(current_user.password, old_password):
+                if new_password == confirm_password:
+                    current_user.password = bcrypt.generate_password_hash(new_password).decode('utf-8')
+                else:
+                    return jsonify({'category': 'warning', 'message': 'Пароли не совпадают'})
+            else:
+                return jsonify({'category': 'warning', 'message': 'Ошибка в старом пароле'})
+
+        db.session.commit()
+
+        return jsonify({'category': 'success', 'message': 'Настройки сохранены'})
+
+    except IntegrityError as e:
+        db.session.rollback()
+        print(str(e))
+        return jsonify({'category': 'danger', 'message': 'Ошибка на сервере'})
+
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        print(str(e))
+        return jsonify({'category': 'danger', 'message': 'Ошибка на сервере'})
+
+    return jsonify({'category': 'danger', 'message': 'Ошибка на сервере'})
